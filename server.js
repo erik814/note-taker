911c9f66047563cb80aed73744ea1ff8f0d6b692
erik814 <erikneafus@gmail.com>
2023-01-30 18:49:38 -0600

    Setup up some of the server.js boilerplate.

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
-const PORT = 3001;
-const app = express();
+const app = express(); 
+const PORT = process.env.PORT || 3001;
+
+// Here we tell Express where our folder is for static assets
+app.use(express.static('public'));
+
+// Here we set up express to properly read and parse form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// route for home page and note page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('api/notes', (req, res) =>{
+    
+})
