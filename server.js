14e1b2d5fc458f166575e3042d412b1710250667
erik814 <erikneafus@gmail.com>
2023-01-31 18:54:30 -0600

    Get complete. Working on Post.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
 const app = express(); 
 const PORT = process.env.PORT || 3001;
@@ -14,8 +15,60 @@ app.use(express.urlencoded({ extended: true }));
 
 // route for home page and note page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-app.get('api/notes', (req, res) =>{
-    
+
+app.get('/api/notes', (req, res) =>{
+    fs.readFile('./db/db.json', 'utf8', (err, data) =>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log('Request to get notes received');
+        }
+    })
 })
+
+
+
+// request to add a note
+app.post('/api/notes', (req, res) =>{
+    console.log('save request received')
+    console.log(`entered post ${req.body}`)
+
+    //if the new note has a title and text, create a new object
+    const {title, text} = req.body;
+    if (title && text){
+        const newNote = {
+            title,
+            text,
+            noteId: uuid()
+        };
+    }
+
+    //get current notes
+    fs.readFile('./db/db.json', 'utf-8', (err, data) =>{
+        if(err){
+            console.log(err);
+        }else{
+            //push the new note into allNote array
+            console.log('data line 55: ' + JSON.parse(data))
+            const allNotes = JSON.parse(data);
+            allNotes.push(newNote);
+
+            //rewrite db.json with new note array
+            fs.writeFile('./db/db.json', JSON.stringify(allNotes), err =>{
+                if(err){
+                    console.log(err)
+                }else{
+                    console.log('Note added')
+                }
+            })
+        }
+    })
+})
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
